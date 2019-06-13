2599e957fe9354f7897d7fc82b614857646327b9
ximo12 <ximo94@hotmail.com>
2019-06-13 09:53:23 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -9,6 +9,53 @@ var level = 0;
 
 var started = false;
 
+// when user click any button
+$(".btn").click(function(){
+  // select the button id and save in variable
+  var userChosenColour = $(this).attr("id");
+  // push the id into user pattern array
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor){
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+function checkAnswer(currentlevel){
+
+  if (gamePattern[currentlevel] === userClickedPattern[currentlevel]){
+    if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    console.log("succes");
+  }else{
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key To Restart");
+    console.log("wrong");
+    startOver();
+  }
+}
 
 function nextSequence(){
   //reset the user clicked pattern for new Level
@@ -30,46 +77,9 @@ $(document).keypress(function(){
   }
 });
 
-function checkAnswer(currentlevel){
-
-  if (gamePattern[currentlevel]===userClickedPattern[currentlevel]){
-    setTimeout(function(){
-      nextSequence();
-    }, 1000);
-    console.log("succes");
-  }else{
-    playSound("wrong");
-    setTimeout(function(){
-      $("body").addClass("game-over");
-    }, 200);
-    $("#level-title").text("Game Over, Press Any Key To Restart");
-    console.log("wrong");
-  }
-}
-
-
-
-// when user click any button
-$(".btn").click(function(){
-  // select the button id and save in variable
-  var userChosenColour = $(this).attr("id");
-  // push the id into user pattern array
-  userClickedPattern.push(userChosenColour);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-
-  checkAnswer(userClickedPattern.lenght-1);
-});
-
-function playSound(name){
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
-}
-
-function animatePress(currentColor){
-  $("#" + currentColor).addClass("pressed");
-
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+function startOver(){
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+  level = 0;
 }
